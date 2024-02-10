f5d32167ce47c68372dd50417e090fc4dc056cf3
coyote95 <marie.guillot.oc@gmail.com>
2024-02-10 21:36:48 +0100

    model user

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    age = models.IntegerField()
+    can_be_contacted = models.BooleanField(default=False)
+    can_data_be_shared = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
