a16534d7584378bb4cfbe8ead47d791b0a69e9c6
coyote95 <marie.guillot.oc@gmail.com>
2024-02-11 23:20:48 +0100

    admin

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from projects.models import Project, Issue, Comment
 
-# Register your models here.
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("name", "description", "type", "author", "created_time", "display_contributors")
+
+    def display_contributors(self, obj):
+        return ", ".join([contributor.username for contributor in obj.contributors.all()])
+
+
+class IssueAdmin(admin.ModelAdmin):
+    list_display = ("name", "description", "project", "author", "priority", "tag", "status", "created_time")
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("text", "issue", "author", "uuid", "created_time")
+
+
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Issue, IssueAdmin)
+
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from users.models import User
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("username", "age", "can_be_contacted", "can_data_be_shared")
+
+
+admin.site.register(User, UserAdmin)
+
